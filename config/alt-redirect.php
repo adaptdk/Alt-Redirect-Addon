a097d9592b9db9f65521c7fdc3356912274e1017
Martin Hesselbæk Olesen <47330347+Tomodo531@users.noreply.github.com>
2025-10-14 16:32:35 +0200

    WEB-1486: Add documentation

--- config/alt-redirect.php
+++ config/alt-redirect.php
@@ -6,7 +6,9 @@ return [
 
 	'events' => [
 		'entry' => [
+			// Rather or not the redirects to the updated entry should be updated to the new slug
 			'update_redirect_to_entry' => false,
+			// Rather or not a redirect should be created from the old slug to the new slug
 			'create_redirect_from_old_to_new_slug' => false,
 		],
 	],
